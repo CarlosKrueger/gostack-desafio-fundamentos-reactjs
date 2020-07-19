2b5828466abdbc4539775beeb317895f5f65cffe
Carlos Krueger <crlalexandrek@gmail.com>
2020-07-19 17:33:44 -0300

    feat: challenge completed

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,25 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.map(fileProps => data.append('file', fileProps.file));
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      setUploadedFiles([]);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const newFiles: FileProps[] = files.map<FileProps>(file => ({
+      file,
+      name: file.name,
+      readableSize: file.size.toString(),
+    }));
+    setUploadedFiles([...uploadedFiles, ...newFiles]);
   }
 
   return (
